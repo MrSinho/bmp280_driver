fd739c819aa32a493ad84a7acc2483a5c1b35080
Anas Boustani <sino3791@gmail.com>
2021-09-18 15:03:21 +0200

    Update pico-sdk_prerequisites.bat

--- scripts/pico-sdk_prerequisites.bat
+++ scripts/pico-sdk_prerequisites.bat
@@ -1,5 +1,5 @@
 echo "deleting line 6 of external/pico-sdk/src/rp2_common/pico_standard_link/CMakeLists.txt"
-cd external/pico-sdk/src/rp2_common/pico_standard_link
+cd ../external/pico-sdk/src/rp2_common/pico_standard_link
 findstr /V "new_delete.cpp" CMakeLists.txt > _CMakeLists.txt
 ren CMakeLists.txt __CMakeLists.txt
 ren _CMakeLists.txt CMakeLists.txt
