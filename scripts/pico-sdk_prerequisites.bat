8fc95cb1c7e900af6d774935cf03a7684a1c5cf8
Anas Boustani <sino3791@gmail.com>
2023-02-04 21:10:03 +0100

    Delete shell and batch scripts

--- scripts/pico-sdk_prerequisites.bat
+++
@@ -1,8 +0,0 @@
-echo "deleting line 6 of external/pico-sdk/src/rp2_common/pico_standard_link/CMakeLists.txt"
-cd ../external/pico-sdk/src/rp2_common/pico_standard_link
-findstr /V "new_delete.cpp" CMakeLists.txt > _CMakeLists.txt
-ren CMakeLists.txt __CMakeLists.txt
-ren _CMakeLists.txt CMakeLists.txt
-del __CMakeLists.txt
-
-pause
--- scripts/pico-sdk_prerequisites.sh
+++
@@ -1,3 +0,0 @@
-echo "deleting line 6 of external/pico-sdk/src/rp2_common/pico_standard_link/CMakeLists.txt"
-sed -i '6d' ../external/pico-sdk/src/rp2_common/pico_standard_link/CMakeLists.txt
-pause
